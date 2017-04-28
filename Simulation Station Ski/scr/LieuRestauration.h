53e14a5d60eb5fc431b271e8aba60d59c05ddf49
cchevalier2 <clementine.chevalier@insa-rouen.fr>
2017-04-28 12:00:30 +0200

    mise a jour doc

--- Simulation Station Ski/scr/LieuRestauration.h
+++ Simulation Station Ski/scr/LieuRestauration.h
@@ -7,13 +7,39 @@
 
 #ifndef SCR_LIEURESTAURATION_H_
 #define SCR_LIEURESTAURATION_H_
+/**
+ * \class LieuRestauration
+ * \brief
+ * \author Clementine Chevalier
+ * \version 1.0
+ * \date avril 2017
+ */
 
 class LieuRestauration {
 	int capaciteResto;
 public:
+	/**
+	* \fn LieuRestauration()
+	* Constructeur par defaut de LieuRestauration
+	*/
+	LieuRestauration();
+	/**
+	 * \fn LieuRestauration(int)
+	 * Constructeur LieuRestauration
+	 * initialise la capacite maximale du LieuRestauration
+	 */
 	LieuRestauration(int);
 	virtual ~LieuRestauration();
+	/**
+	 * \fn getCapaciteResto()
+	 * return int
+	 * Retourne la capacite maximale du LieuRestauration
+	 */
 	int getCapaciteResto();
+	/**
+	 * \fn setCapaciteResto
+	 * modifie la valeur capaciteResto
+	 */
 	void setCapaciteResto(int);
 };
 
