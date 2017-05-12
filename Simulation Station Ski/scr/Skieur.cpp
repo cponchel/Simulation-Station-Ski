43cb38b738cb80c0bd8db7f34fd91db776107aa1
xliu04 <xliu04@gmlin123>
2017-05-12 10:36:26 +0200

    function partir();

--- Simulation Station Ski/scr/Skieur.cpp
+++ Simulation Station Ski/scr/Skieur.cpp
@@ -35,7 +35,6 @@ Skieur::Skieur(string leNom,string lePrenom,int leNiveau,int lHeureArrivee){
 	niveauS=leNiveau;
 	heureArrivee=lHeureArrivee;
 	heureDepart=0;
-	nbPassagesLR=0;
 	tempsTrajet=0;
 	tempsAttente=0;
 	tempsTotalPistes=0;
@@ -232,10 +231,10 @@ void Skieur::determinerArcSuivant(){
 					}
 				}
 			}
-			int temp=rand()%proba.size(); // on choisit au hasard un indice ind du tableau compris entre 0 et tab.size() exclus
+			//int temp=rand()%proba.size(); // on choisit au hasard un indice ind du tableau compris entre 0 et tab.size() exclus
 
 			// mise à jour de l'arc actuel
-			setArcActuel(getArcActuel()->getSuivants()[proba[temp]]); // l'arc actuel correspond à la case d'indice tab[ind] du vecteur arcSuivants
+			setArcActuel(getArcActuel()->getSuivants()[0]); // l'arc actuel correspond à la case d'indice tab[ind] du vecteur arcSuivants
 
 		}
 		else{
@@ -247,6 +246,8 @@ void Skieur::determinerArcSuivant(){
 
 
 	}
+
+
 
 void Skieur::choisirArcSuivant(){
 
@@ -365,6 +366,10 @@ void Skieur::seDeplacer(int instant,  string mode){
 
 
 
+}
+
+void Skieur::partir(){
+	arcActuel=new Arc();
 }
 
 ostream& operator<<( ostream &flux, Skieur const& skieur ){
